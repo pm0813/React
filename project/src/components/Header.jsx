b74f8dce14958c166a604201e792cda92adbc5e4
pm0813 <103161585+pm0813@users.noreply.github.com>
2024-04-18 17:00:29 +0900

    modified:   project/src/assets/scss/section/_header.scss 	modified:   project/src/components/Header.jsx

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -4,37 +4,42 @@ import { IoMdSearch } from "react-icons/io";
 import { IoPersonOutline } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 
-const headderNav = [
+const headerNav = [
   {
     title: "소개",
-    url: "/"
+    url: "/",
+    values: ["소개"]
   },
   {
     title: "도매가",
-    url: "/"
+    url: "/",
+    values: ["표","그래프"]
   },
   {
     title: "메뉴 추천",
-    url: "/"
+    url: "/",
+    values: ["메뉴 추천"]
   },
   {
     title: "마이페이지",
-    url: "/"
+    url: "/",
+    values: ["회원정보수정","문의내역확인"]
   },
   {
-    title: "고객지원",
-    url: "/"
+    title: "커뮤니티",
+    url: "/",
+    values: ["자유게시판","문의하기"]
   }
 ];
 
 const Header = () => {
-  const [slideShow, setSlideShow] = useState(false);
+  const [sideBarShow, setSideBarShow] = useState(false);
   
-  const mobileSlide = (event) => {
+  const mobileSideBar = (event) => {
     event.preventDefault();
     event.stopPropagation();
     
-    setSlideShow(!slideShow);
+    setSideBarShow(!sideBarShow);
   };
 
   return (
@@ -44,39 +49,51 @@ const Header = () => {
           <h1><a href="/">로고</a></h1>
         </div>
         <nav 
-          className={`header__nav ${slideShow ? "show" : ""}`}
+          className={`header__nav ${sideBarShow ? "show" : ""}`}
           role="navigation"
           aria-label="메인 메뉴"
         >
-          <ul>
-            {headderNav.map((nav, key) => (
+          <ul className="dropDown">
+            {headerNav.map((nav, key) => (
               <li key={key}>
                 <a href="{nav.url}">{nav.title}</a>
+                <div className="item">
+                  <ul className="dropDownMenu">
+                    {nav.values.map((value, index) => (
+                      <li key={`${nav.title}-${index}`}>{value}</li>
+                    ))}
+                  </ul>
+                </div>
               </li>
             ))}
           </ul>
         </nav>
         <div className="icon__menu">
           <ul>
-            <li className="search-icon clickOpen">
+            <li 
+              className="searchBar"
+              id="searchBarToggle"
+              role="button"
+            >
               <IoMdSearch />
             </li>
             <li className="login">
               <a href="/"><IoPersonOutline /></a>
             </li>
             <li 
-              className="mobile-menu"
-              id="headerToggle"
+              className="mobileSideBar"
+              id="mobileSideBarToggle"
               aria-controls="primary-menu"
-              aria-expanded={slideShow ? "true" : "false"}
+              aria-expanded={sideBarShow ? "true" : "false"}
               role="button"
               tabIndex="0"
-              onClick={mobileSlide}
+              onClick={mobileSideBar}
             >
-              <a href="/">{slideShow ? <IoClose /> : <IoMenuSharp />}</a>
+              <a href="/">{sideBarShow ? <IoClose /> : <IoMenuSharp />}</a>
             </li>
           </ul>
         </div>
+        
       </div>
     </div>
   );
