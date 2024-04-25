ebec5ea4fb875fe5b8e50cb877815e20d9538f6d
pm0813 <qqkrals0813@naver.com>
2024-04-25 06:42:08 +0000

    modified:   project/src/assets/scss/section/_header.scss 	new file:   project/src/assets/scss/section/_main.scss 	modified:   project/src/assets/scss/setting/_font.scss 	modified:   project/src/assets/scss/setting/_vars.scss 	modified:   project/src/assets/scss/style.scss 	modified:   project/src/components/Footer.jsx 	modified:   project/src/components/Main.jsx

--- project/src/components/Main.jsx
+++ project/src/components/Main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
-const Main = ({ children }) => {
+const Main = () => {
   return(
-    <main id="main" role="main">
-      {children}
+    <main id="main">
+      <section className="section01">
+        <div className="wrap">
+          <div className="sec01_l">
+            <div className="sel_type">
+              <div className="selectbox">
+                
+              </div>
+              <h2> 오늘 관심있는 품목 소비자 가격은?</h2>
+            </div>
+            <div className="tab price">
+              <div className="flex_tit">
+                <p className="price_legend">가격단위 : 원 
+                  <span id="baseDate">기준일 (1999.08.13)</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
